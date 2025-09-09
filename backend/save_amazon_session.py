cce7305b1b91bae107ef6263508d52a15070fe2f
Debangan Deb <140197188+debangan-deb@users.noreply.github.com>
2025-09-10 00:44:07 +0530

    Add files via upload

---
+++ backend/save_amazon_session.py
@@ -0,0 +1,11 @@
+from playwright.sync_api import sync_playwright
+
+with sync_playwright() as p:
+    browser = p.chromium.launch(headless=False)
+    context = browser.new_context()
+    page = context.new_page()
+    page.goto("https://www.amazon.in/ap/signin?openid.pape.max_auth_age=0&openid.return_to=https%3A%2F%2Fwww.amazon.in%2Fyour-account%3Fref_%3Dnav_ya_signin&openid.identity=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0%2Fidentifier_select&openid.assoc_handle=inflex&openid.mode=checkid_setup&openid.claimed_id=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0%2Fidentifier_select&openid.ns=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0")
+    input("Login to Amazon manually, then press ENTER here...")
+    context.storage_state(path="backend/amazon_auth.json")
+    browser.close()
+
